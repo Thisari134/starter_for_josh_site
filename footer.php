e954b710c55e66538ec512ec50dc6cc2d5bf4369
Thisari134 <thisari.dharmaratne@gmail.com>
2024-04-12 20:29:06 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="container-fluid text-center">
 <div class="container"> 
 <div class="row">
-<div class="col-md-6">
+<div class="col-md-6  widgetleft">
   <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a> -->
   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
 endif; ?>
