319a82b9b09a3250cb31071bd339539ef15de209
Thisari134 <thisari.dharmaratne@gmail.com>
2024-04-12 20:03:38 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,17 @@
-
-
 <footer class="container-fluid text-center">
 <div class="container"> 
 <div class="row">
-
 <div class="col-md-6">
-  <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+  <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a> -->
+  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+endif; ?>
 
 </div>
-
 <div class="col-md-6">
     <p>Terms and Conditions</p>
     
     </div>
+
 </div> <!-- row -->
 </div> <!-- container -->    
 </footer>
