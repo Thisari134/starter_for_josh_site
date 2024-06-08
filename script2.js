cd862be9e4ed8a9413580a54dd478c29fa948553
Thisari134 <thisari.dharmaratne@gmail.com>
2024-06-08 10:21:53 +0800

    astronaut

---
+++ script2.js
@@ -0,0 +1,12 @@
+<script>
+  LottieInteractivity.create({
+    player: "#twelfthLottie",
+    mode:"scroll",
+    actions: [
+      {
+        visibility: [0.50, 1.0],
+        type: "play"
+      }
+    ]
+  });
+</script>
