76264d65ab587988b0eb9181af8997813c5fd12a
Thisari134 <thisari.dharmaratne@gmail.com>
2024-06-08 10:22:35 +0800

    edit

--- script1.js
+++ script1.js
@@ -28,15 +28,3 @@ tl.from('.about-text',{
 opacity:0,
 });
 
-<script>
-  LottieInteractivity.create({
-    player: "#twelfthLottie",
-    mode:"scroll",
-    actions: [
-      {
-        visibility: [0.50, 1.0],
-        type: "play"
-      }
-    ]
-  });
-</script>
